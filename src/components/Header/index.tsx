dbb0bb2d85a3bf129fb7a3f0b27d8f9081200a3f
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2025-01-20 21:32:36 -0100

    feat: adding nav and menu for mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,31 @@
 import Image from "next/image"
-import { InstaButton, MenuButton, SpecialButton, StyledButton, StyledHeader, StyledNavbar } from "./style"
+import { ButtonMobile, InstaButton, MenuButton, MenuMobile, NavMobile, SpecialButton, StyledButton, StyledHeader, StyledNavbar } from "./style"
+import { useState, useEffect } from "react";
 
 const Header = () => {
+    const [open, setOpen] = useState(false)
 
-    const scrollToSection = (id: string) => {
+    useEffect(() => {
+        if (open) {
+            document.body.style.overflow = "hidden"; // Bloqueia o scroll
+        } else {
+            document.body.style.overflow = "auto"; // Restaura o scroll
+        }
+    
+        // Limpa o estilo ao desmontar o componente
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [open]);
+
+    const handleButtonClick = (id: string) => {
         const section = document.getElementById(id);
         if (section) {
             section.scrollIntoView({ behavior: "smooth" });
         }
-    };
+
+        setOpen(false)
+    }
 
     return (
         <>
@@ -21,35 +38,89 @@ const Header = () => {
                 />
 
                 <StyledNavbar>
-                    <StyledButton onClick={() => scrollToSection('hero')}>
+                    <StyledButton onClick={() => handleButtonClick('hero')}>
                         Início
                     </StyledButton>
-                    <StyledButton onClick={() => scrollToSection('about')}>
+                    <StyledButton onClick={() => handleButtonClick('about')}>
                         Sobre Nós
                     </StyledButton>
-                    <StyledButton onClick={() => scrollToSection('services')}>
+                    <StyledButton onClick={() => handleButtonClick('services')}>
                         Serviços
                     </StyledButton>
-                    <StyledButton onClick={() => scrollToSection('gallery')}>
+                    <StyledButton onClick={() => handleButtonClick('gallery')}>
                         Galeria
                     </StyledButton>
-                    <SpecialButton onClick={() => scrollToSection('schedule')}>
+                    <SpecialButton onClick={() => handleButtonClick('schedule')}>
                         Agende
                     </SpecialButton>
                 </StyledNavbar>
 
+                <MenuMobile isOpen={open}>
+                    <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}> 
+                        <Image 
+                            src='/images/boni-logo-white.png'
+                            width={140}
+                            height={180}
+                            alt="golden-logo"
+                            style={{margin: '20px auto'}}
+                        />
+                        <NavMobile>
+                            <ButtonMobile onClick={() => handleButtonClick('hero')}>
+                                Início
+                            </ButtonMobile>
+                            <ButtonMobile onClick={() => handleButtonClick('about')}>
+                                Sobre Nós
+                            </ButtonMobile>
+                            <ButtonMobile onClick={() => handleButtonClick('services')}>
+                                Serviços
+                            </ButtonMobile>
+                            <ButtonMobile onClick={() => handleButtonClick('gallery')}>
+                                Galeria
+                            </ButtonMobile>
+                            <ButtonMobile onClick={() => handleButtonClick('schedule')}>
+                                Agende
+                            </ButtonMobile>
+                        </NavMobile>
+                        <div style={{margin: '30px auto'}}>
+                            <a href="https://www.instagram.com/sr.sra.boni/" target="_blank">
+                            
+                                <Image 
+                                    src='/images/instagram-icon.svg'
+                                    width={35}
+                                    height={35}
+                                    alt="golden-logo"
+                                    
+                                />
+                            </a>
+                        </div>
+                    </div>
+
+                    <div style={{position: 'absolute', right: '30px', top: '30px'}} onClick={() => setOpen(!open)}>
+                        <Image 
+                            src='/images/close-icon.svg'
+                            width={50}
+                            height={50}
+                            alt="golden-logo"
+                        />
+
+                    </div>
+
+                </MenuMobile>
+
                 <InstaButton>
+                    <a href="https://www.instagram.com/sr.sra.boni/" target="_blank">
                     
-                    <Image 
-                        src='/images/instagram-icon.svg'
-                        width={30}
-                        height={30}
-                        alt="golden-logo"
-                        
-                    />
+                        <Image 
+                            src='/images/instagram-icon.svg'
+                            width={30}
+                            height={30}
+                            alt="golden-logo"
+                            
+                        />
+                    </a>
                 </InstaButton>
 
-                <MenuButton>
+                <MenuButton onClick={() => setOpen(true)}>
                     <Image 
                         src='/images/menu-icon.svg'
                         width={40}
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -68,8 +68,69 @@ export const InstaButton = styled.div`
 
 export const MenuButton = styled.div`
     display: none;
+    cursor: pointer;
+    background-color: none;
+    transition: 200ms all ease-in-out;
+    padding: 10px;
+    border-radius: 100%;
+    margin-right: -15px;
+    
+    &:hover {
+        background-color:  #301E09;
+    }
 
     @media (max-width: 590px) {
-        display: block;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+    }
+`
+
+
+// MOBILE
+
+export const MenuMobile = styled.div<{ isOpen: boolean }>`
+    height: 100vh;
+    background-color: #301e09;
+    width: 100%;
+    position: fixed; /* Usar fixed garante que o menu cubra toda a tela */
+    top: 0;
+    left: 0;
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    transform: translateY(${({ isOpen }) => (isOpen ? "0" : "100%")});
+    opacity: ${({ isOpen }) => (isOpen ? "1" : "0")};
+    transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+
+`;
+
+export const NavMobile = styled.nav`
+    display: flex;
+    flex-direction: column;
+    margin-top: 32px;
+    width: 100%;
+`
+
+export const ButtonMobile = styled.button`
+    border: none;
+    background-color: transparent;
+    background: none;
+    color: white;
+    width: 100%;
+    padding: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+        background-color: white;
+        color: #301e09;
+    }
+
+    &:active {
+        transform: scale(0.95); /* Cria um efeito de "aperto" */
+        background-color: rgba(250, 250, 250, 0.97); /* Adiciona um fundo para indicar o clique */
     }
 `
